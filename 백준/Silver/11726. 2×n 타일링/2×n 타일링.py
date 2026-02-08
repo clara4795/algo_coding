34ec86f539d20a7e061ff874214e9e25c2c4ba82
gajigajigaza <127067635+gajigajigaza@users.noreply.github.com>
2026-02-08 18:20:38 +0900

    [Silver III] Title: 2×n 타일링, Time: 56 ms, Memory: 35028 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -5,8 +5,8 @@ def dp(num):
     cnt = [0] * (num+1)
     for i in range(num+1):
         if i <= 3: cnt[i] = i
-        else: cnt[i] = cnt[i-1] + cnt[i-2]
+        else: cnt[i] = (cnt[i-1] + cnt[i-2]) % 10007
     return cnt[num]
 
 n = int(input())
-print(dp(n) % 10007)
+print(dp(n))
